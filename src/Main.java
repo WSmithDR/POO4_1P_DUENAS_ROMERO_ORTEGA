10e02e8f8d69f4148637905159292006017d1029
= <=>
2025-06-17 15:34:12 -0500

    feat: configurado entorno inicial del proyecto

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import Demo1.Demo1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Demo1 dm1 = new Demo1();
+    }
+}
